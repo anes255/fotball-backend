543a31ebe75cc684b2a0c4011dd77a38a4218bb3
anes255 <anesgaher3000@gmail.com>
2026-02-03 22:03:30 +0100

    Update matchRoutes.js

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -4,13 +4,16 @@ const matchController = require('../controllers/matchController');
 const { auth, adminAuth } = require('../middleware/auth');
 
 // Public routes
-router.get('/', matchController.getAll);
+router.get('/visible', matchController.getVisible);  // Users see only 24h before
 router.get('/upcoming', matchController.getUpcoming);
-router.get('/:id', matchController.getById);
+router.get('/team/:teamId', matchController.getByTeam);  // Matches for a team
+router.get('/tournament/:tournamentId/visible', matchController.getByTournamentVisible);
 router.get('/:id/can-predict', matchController.canPredict);
-router.get('/tournament/:tournamentId', matchController.getByTournament);
+router.get('/:id', matchController.getById);
 
 // Admin routes
+router.get('/', auth, adminAuth, matchController.getAll);  // Admin sees all
+router.get('/tournament/:tournamentId', auth, adminAuth, matchController.getByTournament);
 router.post('/', auth, adminAuth, matchController.create);
 router.put('/:id', auth, adminAuth, matchController.update);
 router.put('/:id/result', auth, adminAuth, matchController.setResult);
